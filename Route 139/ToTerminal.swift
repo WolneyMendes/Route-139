27c46e4917a2b6cb6e138b601060223cc617576a
Wolney Mendes <wolney.m@gmail.com>
2015-08-15 11:42:11 -0400

    Storyboard has now toTerminal and fromTerminal as TableViewController

---
+++ Route 139/ToTerminal.swift
@@ -0,0 +1,95 @@
+//
+//  ToTerminal.swift
+//  Route 139
+//
+//  Created by Wolney Mendes on 8/15/15.
+//  Copyright (c) 2015 Wolney Mendes. All rights reserved.
+//
+
+import UIKit
+
+class ToTerminal: UITableViewController {
+    // MARK: - Table view data source
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 0
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    /*
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return nil
+    }
+    */
+    
+    /*
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("StopCell", forIndexPath: indexPath) as! UITableViewCell
+    
+    // Configure the cell
+    let location = locations?.locationAtIndex(indexPath.section).stopAt(indexPath.item)
+    
+    cell.textLabel?.text = location!.Name
+    
+    if selectedStop != nil {
+    if location!.Equals(selectedStop!) {
+    tableView.selectRowAtIndexPath(
+    indexPath,
+    animated: true,
+    scrollPosition: UITableViewScrollPosition.None)
+    }
+    }
+    
+    return cell
+    }
+    */
+    
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    // Return NO if you do not want the specified item to be editable.
+    return true
+    }
+    */
+    
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+    if editingStyle == .Delete {
+    // Delete the row from the data source
+    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+    } else if editingStyle == .Insert {
+    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }
+    }
+    */
+    
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+    
+    }
+    */
+    
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    // Return NO if you do not want the item to be re-orderable.
+    return true
+    }
+    */
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+    }
+    */
+}
